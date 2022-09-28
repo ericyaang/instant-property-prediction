82c6138259815aacbced4fa2afda4653d712ad15
ericyaang <eric_yang@hotmail.com>
2022-09-28 18:41:52 -0300

    formated

--- src/scraper_script.py
+++ src/scraper_script.py
@@ -3,18 +3,19 @@ import pandas as pd
 from datetime import datetime as dt
 import json
 
+
 class OlxScraper:
     def __init__(self, pages):
         self.pages = pages
 
     def get_urls(self):
-        centro = get_urls_df('centro', self.pages)
-        continente = get_urls_df('continente', self.pages)
-        leste = get_urls_df('leste', self.pages)
-        Norte = get_urls_df('norte', self.pages)
-        sul = get_urls_df('sul', self.pages)
+        centro = get_urls_df("centro", self.pages)
+        continente = get_urls_df("continente", self.pages)
+        leste = get_urls_df("leste", self.pages)
+        Norte = get_urls_df("norte", self.pages)
+        sul = get_urls_df("sul", self.pages)
         return pd.concat([sul], ignore_index=True)
-    
+
     def save_infos(self, data_path):
         infos = []
         links = self.get_urls()
@@ -24,17 +25,15 @@ class OlxScraper:
                 infos.append(temp)
             except:
                 None
-            print(f'Páginas registradas: {index}', end='\r')
+            print(f"Páginas registradas: {index}", end="\r")
 
-        now_ = dt.now().strftime('%d%m%y-%H%M%S') 
-        with open(data_path +
-          now_ + '.json', 'w', encoding='utf-8') as f:
+        now_ = dt.now().strftime("%d%m%y-%H%M%S")
+        with open(data_path + now_ + ".json", "w", encoding="utf-8") as f:
             json.dump(infos, f, ensure_ascii=False, indent=4)
 
+
 if __name__ == "__main__":
-    n_pages= 1
+    n_pages = 1
     data_dir = "data/raw/olx_"
     web = OlxScraper(n_pages)
     web.save_infos(data_dir)
-
-
